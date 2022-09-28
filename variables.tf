ce37b39d8070626e8c8237890fe0601a06e3fb49
Jeffrey <jeffrey.shively@hashicorp.com>
2022-09-28 15:26:29 -0400

    MVP of code refactor with VPC

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "business_unit" {
+  type = string
+  description = "Business Unit"
+}
+
+variable "network_cidr_block" {
+  type = string
+  description = "Network CIDR Block"
+}
+
+variable "public_cidr_block" {
+  type = string
+  description = "Public CIDR Block from network_cidr_block"
+}
+
+variable "user" {
+  type = string
+  description = "User Name"
+}
+
+variable "region" {
+  type = string
+  description = "AWS region"
+}
