0fbc98c8bd1d661767d1de6eaa4258c56f33a215
Jeffrey <jeffrey.shively@hashicorp.com>
2022-09-28 15:27:41 -0400

    terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "business_unit" {
-  type = string
+  type        = string
   description = "Business Unit"
 }
 
 variable "network_cidr_block" {
-  type = string
+  type        = string
   description = "Network CIDR Block"
 }
 
 variable "public_cidr_block" {
-  type = string
+  type        = string
   description = "Public CIDR Block from network_cidr_block"
 }
 
 variable "user" {
-  type = string
+  type        = string
   description = "User Name"
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "AWS region"
 }
